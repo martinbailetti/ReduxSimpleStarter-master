9775c549f43838587fe76f553ffa0deca24aa0db
Martin <martinbailetti@gmail.com>
2018-04-02 18:48:35 -0500

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+export default function(){
+
+	return[
+		{'title': 'Título 1'},
+		{'title': 'Título 2'},
+		{'title': 'Título 3'},
+		{'title': 'Título 4'},
+		{'title': 'Título 5'}
+
+	];
+
+
+}
